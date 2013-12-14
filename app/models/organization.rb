1d5e092d0c13f452c34045e7928599714670373f
Jon Turnbull <jon.turnbull@gmail.com>
2013-12-14 20:56:14 +0000

    Updated with many of the visual identities

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,5 +1,16 @@
 class Organization < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
+  has_many :programs
+  before_destroy :ensure_no_related_programs
   
-  has_many :programs, dependent: :destroy
+  
+  private
+    def ensure_no_related_programs
+      if programs.empty?
+        return true
+      else
+        errors.add(:base, 'Programs present, please remove first')
+        return false
+      end     
+    end
 end
