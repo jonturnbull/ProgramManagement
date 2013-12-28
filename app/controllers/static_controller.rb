7015fd25d5eb7653273a554fd83f4dd236ca7fd3
Jon Turnbull <jon.turnbull@gmail.com>
2013-12-28 19:08:10 +0000

    Added about page, updated elements on page to HTML5 from tables

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,13 @@
 class StaticController < ApplicationController
+  layout "static", :except => "home" 
+  layout "application", :except => "about" 
+  
+   
   def home
     @programs=Program.count
+  end
+  
+  def about
+    
   end
 end
